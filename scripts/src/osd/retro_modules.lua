5e13361ae4b6b9a48aa7fb4e572ee3edf8fb530f
zhuzhoujenson1981-max <zhuzhoujenson1981@gmail.com>
2026-08-03 14:13:22 +0800

    Update retro_modules.lua

--- scripts/src/osd/retro_modules.lua
+++ scripts/src/osd/retro_modules.lua
@@ -65,6 +65,7 @@ function osdmodulesbuild()
 		MAME_DIR .. "src/osd/modules/diagnostics/diagnostics_module.h",
 		MAME_DIR .. "src/osd/modules/diagnostics/none.cpp",
 		MAME_DIR .. "src/osd/modules/font/font_module.h",
+		MAME_DIR .. "src/osd/modules/font/font_sdl.cpp",
 		MAME_DIR .. "src/osd/modules/font/font_none.cpp",
 		MAME_DIR .. "src/osd/modules/input/assignmenthelper.cpp",
 		MAME_DIR .. "src/osd/modules/input/assignmenthelper.h",
